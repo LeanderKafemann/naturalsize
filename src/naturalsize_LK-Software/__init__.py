58c198bbad9a78dfdc72c7ddf7116e40c1d95b59
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2023-12-26 10:46:02 +0100

    update to v1.0.11

--- src/naturalsize_LK-Software/__init__.py
+++ src/naturalsize_LK-Software/__init__.py
@@ -4,8 +4,10 @@ funcs:
 nsize -- returns human-readable size of file
 about -- returns information about actual release
 randprinter -- prints random signs
-rendprinter_ -- returns callable randprinter-object with adjusted settings
+rendprinter_ -- returns callable randprinter-object with customized settings
 special_starter -- func for special startup
+listToInt -- adds up all integer values of a list
+reverse -- reverses boolean
 """
 def nsize(value: int, comma: int = 2, names: list = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "LK1", "LK2"]):
     """
@@ -13,6 +15,7 @@ def nsize(value: int, comma: int = 2, names: list = ["B", "KB", "MB", "GB", "TB"
     args:
     value -- integer size of file in bytes
     comma -- integer value of maximum ndigits
+    names -- list of human-readable names of 1024-B-stepped size args
     """
     values = [1, 1024]
     for i in range(len(names)-2):
@@ -23,9 +26,9 @@ def nsize(value: int, comma: int = 2, names: list = ["B", "KB", "MB", "GB", "TB"
     return str(round(value/values[idx], ndigits=comma if comma != 0 else None))+" "+names[idx]
 def about():
     """
-    Returns information about your release
+    Returns information about your release and other projects by LK
     """
-    return {"Version":(1, 0, 10), "Author":"Leander Kafemann", date:"25.12.2023", recommend:("Büro by LK", "Verschlüsseler by LK", "flappy bird by LK", "isolated_env by LK"), feedbackTo: "leander@kafemann.berlin"}
+    return {"Version":(1, 0, 11), "Author":"Leander Kafemann", date:"26.12.2023", recommend:("Büro by LK", "Verschlüsseler by LK", "flappy bird by LK", "isolated_env by LK"), feedbackTo: "leander@kafemann.berlin"}
 def randprinter(numb: int = 1000, signs: list = list("abcdefghijklmnopqrstuvwxyzäöüß01234567890#'+*-_.:,;!§$%&/()=?`<>^°"), utf8: bool = False):
     """
     Prints numb random signs
@@ -40,21 +43,47 @@ def randprinter(numb: int = 1000, signs: list = list("abcdefghijklmnopqrstuvwxyz
 def randprinter_(numb: int = 1000, signs:list = list("abcdefghijklmnopqrstuvwxyzäöüß01234567890#'+*-_.:,;!§$%&/()=?`<>^°")):
     """
     Method for getting randprinter method with personalised values.
+    Args are specified at randprinter-method.
     """
-    def a(spam):
+    def a(*arg):
+        """
+        customized randprinter-object
+        """
         randprinter(numb, signs)
-    return a
+    return a; callable
 def special_starter(numb: int = 150000000):
     """
-    Func for special startup:
+    Func for e.g. special startup:
     Function passes numb times. When the user presses ctrl+c,
     True is returned.
     """
     try:
         for i in range(numb):
             pass
-        return False
+        return False; bool
     except KeyboardInterrupt:
-        return True
+        return True; bool
     except:
+        return None; bool
+def listToInt(list_: list[int] = []):
+    """
+    Adds all integer-values of a list together.
+    """
+    n = 0
+    for i in list_:
+        n += i
+    return n; int
+def reverse(boolean: bool = None):
+    """
+    Inverses given boolean
+    """
+    if boolean != None:
+        return True if not boolean else False
+    else:
         return None
+def isInt(toCheck, acceptFloat: bool = False):
+    """
+    Checks whether given value is int
+    acceptFloat -- shall floats also be accepted as ints
+    """
+    return type(toCheck) == int or (type(toCheck) == float and acceptFloat)
